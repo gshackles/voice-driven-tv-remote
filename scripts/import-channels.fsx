315f0bebafa003b393121748518363aa2e910c36
Greg Shackles <gshackles@gmail.com>
2017-07-31 23:07:49 -0400

    add some misc scripts/configs

---
+++ scripts/import-channels.fsx
@@ -0,0 +1,32 @@
+#r "../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+#r "../packages/FSharp.Data.SqlClient/lib/net40/FSharp.Data.SqlClient.dll"
+#r "System.Xml.Linq.dll"
+#r "System.Configuration"
+
+open System.IO  
+open System.Text.RegularExpressions  
+open FSharp.Data  
+open FSharp.Data.SqlClient
+
+type Channels = XmlProvider<"./channels.xml">  
+let doc = Channels.Parse (File.ReadAllText "./channels.xml")
+
+type TvChannel = { XmlTvId: string; FullName: string; DisplayName: string; Number: int }
+
+doc.Channels  
+|> Array.map (fun channel ->
+    match channel.DisplayNames with
+    | [| fullName; displayName; number |] -> Some({ XmlTvId = channel.Id
+                                                    FullName = fullName.String.Value
+                                                    DisplayName = Regex.Replace(displayName.String.Value, "\\-HD$", "", RegexOptions.None)
+                                                    Number = number.Number.Value })
+    | _ -> None
+)
+|> Array.choose id
+|> Array.filter (fun channel -> channel.Number > 500 && channel.Number < 1000)
+|> Array.iter (fun channel ->
+    use cmd = new SqlCommandProvider<"INSERT INTO Channel VALUES (@xmlTvId, @displayName, @fullName, @number)", "name=TVListings">()
+    cmd.Execute(xmlTvId = channel.XmlTvId, displayName = channel.DisplayName, fullName = channel.FullName, number = channel.Number) |> ignore
+
+    printfn "Added channel: %s" channel.DisplayName
+)
